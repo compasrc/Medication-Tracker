03d0be25ccd17ffd3293b7fb0fe69792e2c2f26b
tdnx11 <tdnx11@gmail.com>
2026-03-29 17:02:01 -0500

    Created basic template, added Create/Index/Interaction pages

---
+++ Controllers/MedicationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Medication_Tracker.Controllers
+{
+    public class MedicationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Interactions()
+        {
+            return View();
+        }
+    }
+}
